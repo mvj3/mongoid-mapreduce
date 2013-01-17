31f746770c0f5ba378119b4bf8001ee418c14f1d
David Chen <mvjome@gmail.com>
2013-01-17 17:37:22 +0800

    Fix Mongoid::Fields::Mappings removed in mongoid 3.*

--- lib/mongoid/mapreduce/serialization.rb
+++ lib/mongoid/mapreduce/serialization.rb
@@ -11,10 +11,14 @@ module Mongoid
       def serialize(obj, klass)
         return nil if obj.blank?
         obj = obj.is_a?(Boolean) ? (obj ? 1 : 0) : obj
-        
-        obj = obj.to_s =~ /(^[-+]?[0-9]+$)|(\.0+)$/ ? obj.to_i : obj.to_f
 
+        obj = obj.to_s =~ /(^[-+]?[0-9]+$)|(\.0+)$/ ? obj.to_i : obj.to_f
+        obj.to_json
+=begin
+
+        # Mongoid::Fields::Mappings removed in mongoid 3.*
         Mongoid::Fields::Mappings.for(klass).allocate.serialize(obj)
+=end
       end
 
     end
